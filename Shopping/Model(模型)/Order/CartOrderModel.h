6110e1a4c1eec92daa54d090491a9f229d8871a2
baobao1994 <598537487@qq.com>
2017-05-18 18:29:07 +0800

    change

---
+++ Shopping/Model(模型)/Order/CartOrderModel.h
@@ -0,0 +1,30 @@
+//
+//  OrderModel.h
+//  Shopping
+//
+//  Created by 郭伟文 on 2017/5/11.
+//  Copyright © 2017年 Baobao. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CartOrderModel : NSObject
+
+@property (nonatomic, copy) NSString *objectId;
+@property (nonatomic, copy) NSString *foodId;
+@property (nonatomic, copy) NSString *imageUrl;
+@property (nonatomic, copy) NSString *name;
+@property (nonatomic, copy) NSString *price;
+@property (nonatomic, copy) NSString *foodPrice;
+@property (nonatomic, assign) NSInteger count;
+@property (nonatomic, assign) BOOL isCoupon;
+@property (nonatomic, copy) NSString *couponPrice;
+@property (nonatomic, assign) NSInteger couponCount;
+//@property (nonatomic, assign) float sendPrice;//配送费
+//@property (nonatomic, assign) BOOL isUseCoupon;//是否使用优惠券
+//@property (nonatomic, copy) NSString *couponName;//优惠券名称
+
+
+- (id)initWithDictionary:(NSDictionary *)dic;
+
+@end
